d58d13a20318bdb359e93beacd6f81ac3ecea8a5
Lezend <31857579+thelezend@users.noreply.github.com>
2024-11-25 00:39:42 +0530

    change idl imports to target folder

--- web/src/components/codecracy/program-export.ts
+++ web/src/components/codecracy/program-export.ts
@@ -1,6 +1,6 @@
 import { AnchorProvider, Program, web3 } from "@coral-xyz/anchor";
-import CodecracyIDL from "./idl/codecracy.json";
-import type { Codecracy } from "./idl/types";
+import CodecracyIDL from "../../../../target/idl/codecracy.json";
+import type { Codecracy } from "../../../../target/types/codecracy";
 
 // Re-export the generated IDL and type
 export { Codecracy, CodecracyIDL };
